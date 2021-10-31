2e93137373d023b72132d9414ecd121a2b3b0b9d
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2021-10-31 22:30:29 +0700

    Allow CORS On API

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,58 +4,49 @@ defined('BASEPATH') OR exit('You are not authorized to direct script access');
 
 
 class Api extends CI_Controller {
+	public function __construct() 
+ 	{
+    	parent::__construct(); 
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST');
+		header("Access-Control-Allow-Headers: X-Requested-With");
+ 	} 
     
 
 	public function getslider()
 	{
-		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Methods: GET, POST');
-		header("Access-Control-Allow-Headers: X-Requested-With");
-	    if($this->input->is_ajax_request()){
-    		$url = 'https://www.astra-honda.com/api/v1/page/type/home';
-    		echo $this->astra->call($url);
-	    } else {
-	        exit('You are not authorized to direct script access');
-	    }
+	    
+		$url = 'https://www.astra-honda.com/api/v1/page/type/home';
+		echo $this->astra->call($url);
+	    
 	}
 	
 	public function getcategory($id = NULL)
 	{
-	    if($this->input->is_ajax_request()){
-	        if ($id == NULL){
-        		$url = 'https://www.astra-honda.com/api/v1/product/category';
-        		echo $this->astra->call($url);
-	        } else {
-        		$url = 'https://www.astra-honda.com/api/v1/product?category='.$id;
-        		echo $this->astra->call($url);
-	        }
-	    } else {
-	        exit('You are not authorized to direct script access');
-	    }
+		if ($id == NULL){
+			$url = 'https://www.astra-honda.com/api/v1/product/category';
+			echo $this->astra->call($url);
+		} else {
+			$url = 'https://www.astra-honda.com/api/v1/product?category='.$id;
+			echo $this->astra->call($url);
+		}
 	}
 	
 	public function getproduct($idcategory = NULL, $limit)
 	{
-	    if($this->input->is_ajax_request()){
-    		$url = 'https://www.astra-honda.com/api/v1/product?limit='.$limit.'&home=1&category='.$idcategory;
-    		echo $this->astra->call($url);
-	    } else {
-	        exit('You are not authorized to direct script access');
-	    }
+		$url = 'https://www.astra-honda.com/api/v1/product?limit='.$limit.'&home=1&category='.$idcategory;
+		echo $this->astra->call($url);
 	}
 	
 	public function getdetail($slug = NULL)
     {
-        if($this->input->is_ajax_request()){
-            if ($slug == NULL){
-        		$url = 'https://www.astra-honda.com/api/v1/product';
-        		echo $this->astra->call($url);
-            } else {
-                $url = 'https://www.astra-honda.com/api/v1/product/'.$slug;
-        		echo $this->astra->call($url);
-            }
-        } else {
-            exit('You are not authorized to direct script access');
-        }
+        
+		if ($slug == NULL){
+			$url = 'https://www.astra-honda.com/api/v1/product';
+			echo $this->astra->call($url);
+		} else {
+			$url = 'https://www.astra-honda.com/api/v1/product/'.$slug;
+			echo $this->astra->call($url);
+		}
     }
 }
