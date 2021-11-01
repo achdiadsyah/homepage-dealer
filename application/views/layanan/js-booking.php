104328191a4c60e2deb51315441e8556cc62ba91
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2021-11-02 03:32:27 +0700

    Fix Ajax

--- application/views/layanan/js-booking.php
+++ application/views/layanan/js-booking.php
@@ -1,34 +1,34 @@
 
-<script type="text/javascript">
-  
-    function save()
-    {
-        $('#btnSave').attr('disabled',true);
-        $('#btnSave').html('<i class="fas fa-sync fa-spin"></i> Sending...');
-          
-        var url = "https://app.hondalambarona.id/api/bookingservis";
-        
-        // ajax adding data to database
-        $.ajax({
-            url : url,
-            type: "POST",
-            data: $('#form-booking-servis').serialize(),
-            dataType: "JSON",
-            success: function(data)
-            {
-                if(data.status = true) /
+    <script type="text/javascript">
+    
+        function save()
+        {
+            $('#btnSave').attr('disabled',true);
+            $('#btnSave').html('<i class="fas fa-sync fa-spin"></i> Sending...');
+            
+            var url = "https://app.hondalambarona.id/api/bookingservis";
+            
+            // ajax adding data to database
+            $.ajax({
+                url : url,
+                type: "POST",
+                data: $('#form-booking-servis').serialize(),
+                dataType: "JSON",
+                success: function(data)
                 {
-                    mySwalalert('Berhasil Mengirim Data', 'success');
+                    if(data.status = true)
+                    {
+                        mySwalalert('Berhasil Mengirim Data', 'success');
+                        $('#btnSave').html('Submit');
+                        $('#btnSave').attr('disabled',false);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown)
+                {
+                    mySwalalert('Gagal Menyimapan Data', 'danger');
                     $('#btnSave').html('Submit');
                     $('#btnSave').attr('disabled',false);
                 }
-            },
-            error: function (jqXHR, textStatus, errorThrown)
-            {
-                mySwalalert('Gagal Menyimapan Data', 'danger');
-                $('#btnSave').html('Submit');
-                $('#btnSave').attr('disabled',false);
-            }
-        });
-    }
-</script>
+            });
+        }
+    </script>
