aa662527c924d26aa3623928b6f20ded8d2b295f
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2021-11-07 02:07:58 +0700

    Add Clear Form Function

--- application/views/layanan/js-booking.php
+++ application/views/layanan/js-booking.php
@@ -24,6 +24,7 @@
                             mySwalalert('Berhasil Mengirim Data', 'success');
                             $('#btnSave').html('Submit');
                             $('#btnSave').attr('disabled',false);
+                            $('#form-booking-servis')[0].reset();
 
                         } else if(data.status = false){
 
